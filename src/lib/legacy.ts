3e0a1ba38b4b917329e83ad27b13eacaf2de420a
singularapp <terrence@singulardex.com>
2024-09-03 15:50:43 -0400

    change base url

--- src/lib/legacy.ts
+++ src/lib/legacy.ts
@@ -1371,7 +1371,7 @@ export function getAppBaseUrl() {
     return "http://localhost:3011/#";
   }
 
-  return "https://singulardex.com/#";
+  return "https://dapp.singulardex.com/#";
 }
 
 export function getRootShareApiUrl() {
