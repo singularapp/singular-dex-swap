2e491636f7fa0a20c086dbaafac5a31b5cf19f76
singularapp <terrence@singulardex.com>
2024-09-12 11:19:12 -0400

    add firebase

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -36,11 +36,36 @@ import ExternalLink from "components/ExternalLink/ExternalLink";
 import { sendPendingOrderTxnErrorMetric } from "lib/metrics";
 import { AppRoutes } from "./AppRoutes";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 // @ts-ignore
 if (window?.ethereum?.autoRefreshOnNetworkChange) {
   // @ts-ignore
   window.ethereum.autoRefreshOnNetworkChange = false;
 }
+
+// --------------------------------
+// Firebase init ------------------
+// --------------------------------
+const FIREBASE_API_KEY = import.meta.env.VITE_APP_FIREBASE_API_KEY;
+const FIREBASE_APP_ID = import.meta.env.VITE_APP_FIREBASE_APP_ID;
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: "singular-dex.firebaseapp.com",
+  projectId: "singular-dex",
+  storageBucket: "singular-dex.appspot.com",
+  messagingSenderId: "28932172976",
+  appId: FIREBASE_APP_ID,
+  measurementId: "G-RTQ6SCZKPC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// --------------------------------
+// --------------------------------
 
 function App() {
   const signer = useEthersSigner();
